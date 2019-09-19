9fab683d8e18212ef0e5e607fcebe7b1a5787461
Zachary Schulz <zjschulz@gmail.com>
2019-09-19 23:56:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   array = []
-    sentence.split(" ").each do |x|
-    array << x.reverse
+    sentence.split(" ").map do |x|
+    array << "#{x.reverse} "
   end
   array.join
 end
